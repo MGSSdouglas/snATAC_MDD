280a002528042fe375699cf86a5acd2765baa633
Anjali Chawla <60046859+AnjaliC4@users.noreply.github.com>
2023-10-19 00:53:21 -0400

    Update 1_create_snATAC_project.sh

--- 2_clustering/snATAC_project_clustering/bash_scripts/1_create_snATAC_project.sh
+++ 2_clustering/snATAC_project_clustering/bash_scripts/1_create_snATAC_project.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=0-8:00:00
 #SBATCH -N 1
-#SBATCH --output=/home/anjali5/scratch/graham_merge_analysis/R_scripts/rscript1.out
-#SBATCH --error=/home/anjali5/scratch/graham_merge_analysis/R_scripts/rscript1.er
+#SBATCH --output=/home/anjali5/scratch/graham_merge_analysis/R_scripts/step1.out
+#SBATCH --error=/home/anjali5/scratch/graham_merge_analysis/R_scripts/step2.er
 #SBATCH --mail-user=anjali.chawla@mail.mcgill.cae
 #SBATCH --mail-type=ALL
 
